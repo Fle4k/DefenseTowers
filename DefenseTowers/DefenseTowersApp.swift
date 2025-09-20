07c421b32820a1fb29e5406d13816cdd5c40c632
Shahin shokoui <shahin@shahin.de>
2025-09-21 00:36:32 +0200

    Initial Commit

---
+++ DefenseTowers/DefenseTowersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DefenseTowersApp.swift
+//  DefenseTowers
+//
+//  Created by Shahin on 21.09.25.
+//
+
+import SwiftUI
+
+@main
+struct DefenseTowersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DefenseTowersTests/DefenseTowersTests.swift
@@ -0,0 +1,17 @@
+//
+//  DefenseTowersTests.swift
+//  DefenseTowersTests
+//
+//  Created by Shahin on 21.09.25.
+//
+
+import Testing
+@testable import DefenseTowers
+
+struct DefenseTowersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
